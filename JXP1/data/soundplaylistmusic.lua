19462105aaac44343ae20f4cd339758bd3ed53eb
Jessie Kirk <thejessiekirk@proton.me>
2023-07-01 12:55:15 +0100

    Initial commit.

---
+++ JXP1/data/soundplaylistmusic.lua
@@ -0,0 +1,29 @@
+playlist = 
+{
+  tracks =
+  {
+		"ambient_ingame_01",
+    "ambient_ingame_02",
+    "ambient_ingame_04",
+    "battle_ingame_01",
+    "music_acid",
+    "music_coming_of_chaos",
+    "music_eldarthemev2",
+    "music_evil_isador_theme",
+    "music_force_commander_theme",
+    "music_invasion_theme",
+    "music_march_of_the_emperor",
+    "music_ork_theme",
+    "music_tank_march",
+    "music_theme_chaos_01",
+    "music_theme_spacemarines_01",
+    "music_urban_wasteland",
+    "music_warhammer40ktheme",
+    "stinger_tensedanger",
+  },
+
+  silence_min = 0.0,
+  silence_max = 0.0,
+
+  order = false,
+}
